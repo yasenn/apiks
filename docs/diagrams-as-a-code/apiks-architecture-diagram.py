54f4a115a4d6c6bda68b1f4f54a9b4748fed4e0c
yasenn <12043095+yasenn@users.noreply.github.com>
2021-01-10 19:57:35 +0400

    diagram

--- docs/diagrams-as-a-code/apiks-architecture-diagram.py
+++ docs/diagrams-as-a-code/apiks-architecture-diagram.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
-from diagrams import Cluster,Diagram
+from diagrams import Cluster, Diagram
+from diagrams.aws.database import RDS
+from diagrams.aws.general import User
+from diagrams.aws.network import APIGateway
+from diagrams.k8s.compute import Pod
+
+with Diagram("apiks", show=False):
+    topic = User("client")
+
+    with Cluster("AWS"):
+        with Cluster("Node"):
+            pod = Pod("Nginx")
+            kong = APIGateway("Kong")
+            flask = Pod("Flask")
+
+        RDS = RDS("RDS PostgreSQL")
+
+    topic >> pod
+    pod >> kong >>flask
+    flask >> RDS
